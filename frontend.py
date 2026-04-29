91dc199ce00a0687ccae76c2edce1f9b80ab27fe
Evelyn <eyohanna@asu.edu>
2026-04-29 01:47:42 -0700

    added needs buttons to the layout

--- frontend.py
+++ frontend.py
@@ -41,6 +41,78 @@ def App():
     yes_button.setFixedSize(400, 200)
  
 
+    hi_button = QPushButton("Hi")
+    hi_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    hi_button.setFixedSize(400, 200)
+
+    bye_button = QPushButton("Bye")
+    bye_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    bye_button.setFixedSize(400, 200)
+
+    help_button = QPushButton("Help")
+    help_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    help_button.setFixedSize(400, 200)
+
+    bathroom_button = QPushButton("Bathroom")
+    bathroom_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    bathroom_button.setFixedSize(400, 200)
+
+    hungry_button = QPushButton("Hungry")
+    hungry_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    hungry_button.setFixedSize(400, 200)
+
+    thirsty_button = QPushButton("Thirsty")
+    thirsty_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    thirsty_button.setFixedSize(400, 200)
+
     i_button = QPushButton("I")
     i_button.setStyleSheet("""
     background-color: #ffd9cc;
@@ -52,14 +124,36 @@ def App():
 """)
     
     i_button.setFixedSize(400, 200)
+
+    need_button = QPushButton("Need")
+    need_button.setStyleSheet("""
+    background-color: #ffd9cc;
+    color: black;
+    font-size: 36px;
+    border-radius: 10px;
+                            
+                        
+""")
+    
+    need_button.setFixedSize(400, 200)
    
     layout.setContentsMargins(10, 10, 10, 10)
     
 
     layout.addWidget(no_button, 0, 0)
     layout.addWidget(yes_button, 0, 1)
-    layout.addWidget(i_button, 1, 0)
-    layout.setSpacing(5)
+    layout.addWidget(hi_button, 1, 0)
+    layout.addWidget(bye_button, 1, 1)
+    layout.addWidget(help_button, 2, 0)
+    layout.addWidget(bathroom_button, 2, 1)
+    layout.addWidget(hungry_button, 3, 0)
+    layout.addWidget(thirsty_button, 3, 1)
+    layout.addWidget(i_button, 3, 2)
+    layout.addWidget(need_button, 3, 3)
+
+    layout.setSpacing(10)
+
+
 
 
 
